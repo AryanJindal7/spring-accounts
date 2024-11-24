80305e192798caef368f32c1a7a0349a04c0ab87
AryanJindal7 <aryanjindal103@gmail.com>
2024-11-24 12:29:42 +0530

    Updating code with config changes and webhook

--- src/main/java/com/spring/accounts/AccountsApplication.java
+++ src/main/java/com/spring/accounts/AccountsApplication.java
@@ -1,14 +1,17 @@
 package com.spring.accounts;
 
+import com.spring.accounts.dto.AccountContactInfoDTO;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef="auditAwareImpl")
+@EnableConfigurationProperties(value = AccountContactInfoDTO.class)
 @OpenAPIDefinition(info = @Info(title = "Account microservice documentation" ,version = "v1", contact = @Contact(
 		name = "Aryan",email = "aryanjindal103@gmail.com"
 )))
--- src/main/java/com/spring/accounts/controller/AccountController.java
+++ src/main/java/com/spring/accounts/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.spring.accounts.controller;
 
 import com.spring.accounts.constants.AccountsConstants;
+import com.spring.accounts.dto.AccountContactInfoDTO;
 import com.spring.accounts.dto.CustomerDto;
 import com.spring.accounts.dto.ErrorResponseDto;
 import com.spring.accounts.dto.ResponseDto;
@@ -12,7 +13,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,16 +24,22 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1",produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
 @Tag(name = "CRUD API for account management", description = "CRUD API's to create, fetch , update and delete the records")
 public class AccountController {
 
+    @Autowired
     private IAccountsService iAccountsService;
+    @Value("${build.version}")
+    private String buildVersion;
+    @Autowired
+    private Environment environment;
+    @Autowired
+    private AccountContactInfoDTO accountContactInfoDTO;
+
     @PostMapping("/create")
     @Operation(summary = "Create Account Rest API",description = "Create Customers and Bank Account")
     @ApiResponse(responseCode = "201",description = "HTTP Status - CREATED")
-
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDTO){
         iAccountsService.createAccount(customerDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsConstants.STATUS_201,AccountsConstants.MESSAGE_201));
@@ -63,7 +72,6 @@ public class AccountController {
     @DeleteMapping("/delete")
     @Operation(summary = "Fetch Account Rest API",description = "Delete Customers and Bank Account using mobile number")
     @ApiResponse(responseCode = "200",description = "HTTP Status - OK",content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
-
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam     @Pattern(regexp = "(^[0-9]{10})",message = "Mobile Number must be 10 digits")
                                                                 String mobileNumber){
         boolean isUpdated=iAccountsService.deleteAccount(mobileNumber);
@@ -75,5 +83,26 @@ public class AccountController {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(AccountsConstants.STATUS_417,AccountsConstants.MESSAGE_417_DELETE));
         }
 
+    }
+
+    @Operation(summary = "Fetch Account Rest API Build version",description = "Fetch Build details")
+    @ApiResponse(responseCode = "200",description = "HTTP Status - OK")
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo(){
+        return ResponseEntity.ok(buildVersion);
+    }
+
+    @Operation(summary = "Fetch Account Rest API Java version",description = "Fetch Java version details")
+    @ApiResponse(responseCode = "200",description = "HTTP Status - OK")
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion(){
+        return ResponseEntity.ok(environment.getProperty("MAVEN_HOME"));
+    }
+
+    @Operation(summary = "Fetch Account Rest API Contact Info",description = "Fetch contact info details")
+    @ApiResponse(responseCode = "200",description = "HTTP Status - OK")
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountContactInfoDTO> getContactInfo(){
+        return ResponseEntity.ok(accountContactInfoDTO);
     }
 }
